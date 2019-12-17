d59256a67ab1208b7597c7a3a4871b041b2f3d5c
Perry Kundert <perry@hardconsulting.com>
2019-12-17 14:22:20 -0800

    Update  buildDna, nix-shell model

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import ./pkgs.nix {} }:
+{ pkgs ? import ./pkgs.nix, shell ? false }:
 
 with pkgs;
 
@@ -8,6 +8,8 @@ in
 
 {
   happ-example = buildDNA {
+    inherit shell;
+
     name = "happ-example";
     src = gitignoreSource ./.;
 
